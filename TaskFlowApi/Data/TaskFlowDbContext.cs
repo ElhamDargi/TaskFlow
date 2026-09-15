fd6fe11763d5e10872071be7a4c48b38770ce89a
ElhamDrg <elham.dargi@gmail.com>
2026-09-15 12:51:28 +0200

    EF Core setup and initial migration

---
+++ TaskFlowApi/Data/TaskFlowDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TaskFlowApi.Data;
+
+public class TaskFlowDbContext : DbContext
+{
+    public TaskFlowDbContext(DbContextOptions<TaskFlowDbContext> options) : base(options)
+    {
+    }
+}
